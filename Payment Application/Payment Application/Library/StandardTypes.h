bcd8a200d8c42bad56719774a0f22366716387dc
Ahmed Alaa <111080073+ahmedelahmmar@users.noreply.github.com>
2022-09-14 16:29:10 +0200

    Initial commit

---
+++ Payment Application/Payment Application/Library/StandardTypes.h
@@ -0,0 +1,29 @@
+#ifndef _STANDARDTYPES_H
+#define _STANDARDTYPES_H
+
+typedef unsigned char           uint8_t;
+typedef signed char             sint8_t;
+
+typedef unsigned short          uint16_t;
+typedef signed short            sint16_t;
+
+typedef unsigned long           uint32_t;
+typedef signed long             sint32_t;
+
+typedef unsigned long long      uint64_t;
+typedef signed long long        sint64_t;
+
+typedef float                   f32_t;
+
+typedef double                  f64_t;
+
+typedef long double             f128_t;
+
+typedef enum
+{
+    False,
+    True
+
+} bool;
+
+#endif
---
+++ Payment Application/Payment Application/main.c
@@ -0,0 +1,8 @@
+
+#include "Application/application.h"
+
+int main(void)
+{
+
+	return 0;
+}
